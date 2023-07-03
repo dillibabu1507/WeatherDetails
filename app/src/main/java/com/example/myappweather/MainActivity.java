7f9088c67e05da7f000bc993275a32dc42eb29ab
Babu <sivanadhan026@gmail.com>
2023-07-04 00:06:26 +0530

    The details of weather of the entered location is successfully displayed.

---
+++ app/src/main/java/com/example/myappweather/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.myappweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText enterCity;
+    TextView displyResult;
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "e53301e27efa0b66d05045d91b2742d3";
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterCity = findViewById(R.id.enterCityName);
+        displyResult = findViewById(R.id.resultDisplay);
+    }
+
+    public void detailsOfWeather(View view) {
+        String tempUrl = "";
+        String city = enterCity.getText().toString().trim();
+        if (city.equals("")) {
+            displyResult.setText("City field can not be empty!");
+        } else {
+            tempUrl = url + "?q=" + city + "&appid=" + appid;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String output = "";
+                Log.d("response",response);
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                    JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                    String description = jsonObjectWeather.getString("description");
+                    JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                    double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                    double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                    float pressure = jsonObjectMain.getInt("pressure");
+                    int humidity = jsonObjectMain.getInt("humidity");
+                    JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                    String wind = jsonObjectWind.getString("speed");
+                    JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                    String clouds = jsonObjectClouds.getString("all");
+                    JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                    String countryName = jsonObjectSys.getString("country");
+                    String cityName = jsonResponse.getString("name");
+                    displyResult.setTextColor(Color.rgb(68, 134, 199));
+                    output += "Current weather of " + cityName + " (" + countryName + ")"
+                            + "\n Temp: " + df.format(temp) + " °C"
+                            + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                            + "\n Humidity: " + humidity + "%"
+                            + "\n Description: " + description
+                            + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                            + "\n Cloudiness: " + clouds + "%"
+                            + "\n Pressure: " + pressure + " hPa";
+                    displyResult.setText(output);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+
+}
